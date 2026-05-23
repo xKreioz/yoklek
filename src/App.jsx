8858359b39d9ba217765d425a06222e23a25560c
igottagofast <pathcharapisit@gmail.com>
2026-05-23 18:25:43 +0700

    add css and path in app.jsx part  of statistics and verify page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Splash from './pages/Splash';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import Notification from './pages/Notification';
-import Profile from './pages/Profile';
-import Storage from './pages/Storage';
-import Record from './pages/Record';
-import { Layout } from './components/Layout';
-import { TopNavOnlyLayout } from './components/TopNavOnlyLayout';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Splash from "./pages/Splash";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Notification from "./pages/Notification";
+import Profile from "./pages/Profile";
+import Storage from "./pages/Storage";
+import Record from "./pages/Record";
+import Verify from "./pages/Verify";
+import Statistics from "./pages/Statistics";
+import { Layout } from "./components/Layout";
+import { TopNavOnlyLayout } from "./components/TopNavOnlyLayout";
 
 function App() {
   return (
@@ -19,12 +26,14 @@ function App() {
         <Route path="/landing" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
+
         {/* Protected routes (with Layout) */}
         <Route element={<Layout />}>
           <Route path="/home" element={<Home />} />
           <Route path="/storage" element={<Storage />} />
           <Route path="/record" element={<Record />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/statistics" element={<Statistics />} />
         </Route>
 
         {/* Routes with TopNav only */}
